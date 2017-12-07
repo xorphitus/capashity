d7bafd5764fc1a84faa71e100ff69d15ee2b6e9f
xorphitus <xorphitus@gmail.com>
2017-12-08 00:09:42 +0900

    とりあえずDBに接続できる

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [integrant "0.6.1"]
+                 [org.clojure/java.jdbc "0.7.3"]
+                 [mysql/mysql-connector-java "5.1.45"]]
   :main ^:skip-aot capashity.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/capashity/core.clj
+++ src/capashity/core.clj
@@ -1,5 +1,14 @@
 (ns capashity.core
-  (:gen-class))
+  (:gen-class)
+  (:require [integrant.core :as ig]
+            [clojure.java.jdbc :as jdbc]))
+
+(def mysql-db {:dbtype   "mysql"
+               :dbname   "test_db"
+               :user     "root"
+               :password "password"})
+
+(jdbc/query mysql-db "SELECT 1")
 
 (defn -main
   "I don't do a whole lot ... yet."
