aa8688dd6b90a46a9648cb1814d1b7299ec60821
xorphitus <xorphitus@gmail.com>
2017-12-08 02:26:32 +0900

    add record count histories

--- src/capashity/core.clj
+++ src/capashity/core.clj
@@ -24,6 +24,26 @@
     (map (fn[table] {:name table
                      :count (count-rows table)}))))
 
+(def row-counts-histories (atom []))
+
+(defn measure [event-name]
+  (swap! row-counts-histories #(conj % {:event event-name
+                                        :counts (count-for-tables)})))
+
+(defn sub-counts [a b]
+  ;; TODO subtract counts
+  b)
+
+(defn sum-up [data]
+  (->> data
+       (partition 2 1)
+       (map (fn[datum]
+              (let [prev (first datum)
+                    next (last datum)
+                    event (:event next)]
+                {:event event
+                 :counts (sub-counts (:counts prev) (:counts prev))})))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
