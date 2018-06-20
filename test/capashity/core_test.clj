9ce796508221f0dbaf6a936894864276eaddccd5
xorphitus <xorphitus@gmail.com>
2018-06-21 01:29:17 +0900

    write a test (just one case)

--- test/capashity/core_test.clj
+++ test/capashity/core_test.clj
@@ -1,7 +1,20 @@
 (ns capashity.core-test
   (:require [clojure.test :refer :all]
-            [capashity.core :refer :all]))
+            [capashity.core :refer :all]
+            [capashity.mock-server :as mock-server]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def db {:dbtype   "mysql"
+         :dbname   "test_db"
+         :host     "127.0.0.1"
+         :user     "root"
+         :password "password"})
+
+(use-fixtures :once
+  (fn [f]
+    (mock-server/construct-db db)
+    (f)
+    (mock-server/destruct-db db)))
+
+(deftest test-get-tables
+  (testing "get tables"
+    (is (= (get-tables db) ["fruit" "vegetable"]))))
